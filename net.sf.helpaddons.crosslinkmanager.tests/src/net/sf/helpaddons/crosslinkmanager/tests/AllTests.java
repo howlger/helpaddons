f696ba2b0796791d71cb156146aec234fc2a4b29
voormann <voormann@7a88320c-b3b6-4665-86ba-89fc97229aea>
2011-06-24 10:54:53 +0000

    Cross-Link Manager: cross-links must be uncommented because to use documentation plug-ins also without Cross-Link Manager: <!--a href="topic.htm"-->link text<!--/a-->

--- net.sf.helpaddons.crosslinkmanager.tests/src/net/sf/helpaddons/crosslinkmanager/tests/AllTests.java
+++ net.sf.helpaddons.crosslinkmanager.tests/src/net/sf/helpaddons/crosslinkmanager/tests/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Holger Voormann and others.
+ * Copyright (c) 2010, 2011 Holger Voormann and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,8 +17,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ AbstractHrefResolverTest.class,
                 CrossLinksResolvedInputStreamTest.class,
-                PoolRegistryTest.class,
-                HelpContentWithCrossLinksTest.class})
+                HelpContentWithCrossLinksTest.class,
+                PoolRegistryTest.class})
 public class AllTests {
 
 }
--- net.sf.helpaddons.crosslinkmanager.tests/src/net/sf/helpaddons/crosslinkmanager/tests/HelpContentWithCrossLinksTest.java
+++ net.sf.helpaddons.crosslinkmanager.tests/src/net/sf/helpaddons/crosslinkmanager/tests/HelpContentWithCrossLinksTest.java
@@ -1,16 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Holger Voormann and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Holger Voormann - initial API and implementation
+ *******************************************************************************/
 package net.sf.helpaddons.crosslinkmanager.tests;
 
 import static org.junit.Assert.*;
-
-import java.io.InputStream;
-import java.util.Locale;
-
-import net.sf.helpaddons.crosslinkmanager.CrossLinkManagerPlugin;
 import net.sf.helpaddons.crosslinkmanager.HelpContentWithCrossLinks;
-import net.sf.helpaddons.crosslinkmanager.IStaticHelpContent;
 
-import org.eclipse.help.HelpSystem;
-import org.junit.Before;
 import org.junit.Test;
 
 public class HelpContentWithCrossLinksTest {
@@ -21,7 +23,6 @@ public class HelpContentWithCrossLinksTest {
         assertTrue(HelpContentWithCrossLinks.hasHtmlFileExtension("test.html"));
         assertTrue(HelpContentWithCrossLinks.hasHtmlFileExtension("test.xhtml"));
         assertFalse(HelpContentWithCrossLinks.hasHtmlFileExtension("xhtml.gif"));
-
 
         // query and anchor
         assertTrue(HelpContentWithCrossLinks.hasHtmlFileExtension("test.htm?query=test"));
